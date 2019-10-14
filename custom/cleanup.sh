a873deb6193871658a2b985b3597d88b6f40390e
root <root@ip-172-31-89-204.ec2.internal>
2019-10-14 12:22:18 +0000

    added existing mysql feature

--- custom/cleanup.sh
+++ custom/cleanup.sh
@@ -6,8 +6,16 @@
 # MYSQL_ROOT_PASS
 # MYSQL_ROOT_USER
 # INIT_DB_SCRIPT_PATH = /tmp/secrets/.db-final-script
+# MYSQL_DATA_EXISTS = false
 #
 ### END OF TOKENS
+
+cat <<EOF > /tmp/reset.sql
+UPDATE mysql.user set password='' where user='root';
+flush privileges;
+quit;
+
+EOF
 
 timerStart=$SECONDS
 timeOut=300
@@ -19,9 +27,19 @@ until [[ `netstat -tulpn | grep ##MARIADB_PORT## | grep LISTEN` ]];
     if [[ $(( SECONDS - timerStart )) -gt $timeOut ]]; then \
       echo "Timeout has reached" && exit 1;
     fi
+
+    if ##MYSQL_DATA_EXISTS##; then \
+      if [[ -S /tmp/mysql.sock && -f /tmp/reset.sql ]]; then \
+        echo "Resetting Root Password.." 
+        mysql -u##MYSQL_ROOT_USER## -P0 --socket /tmp/mysql.sock -p##MYSQL_ROOT_PASS## < /tmp/reset.sql
+        rm -f /tmp/reset.sql
+        echo "Password has been resetted..."
+      fi
+    fi
     
   done
 echo "Port ##MARIADB_PORT## has opened!"
+rm -f /tmp/reset.sql
 
 /usr/bin/expect -c '
 set MYSQL_SECURE_INSTALL_PASS "##MYSQL_ROOT_PASS##"
